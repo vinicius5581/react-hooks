ac556393eca91d2351cb356e53acd5de2dd8617e
Vinicius Santana <santana@Viniciuss-MacBook-Pro.local>
2018-11-26 23:14:11 -0800

    Updates

---
+++ precache-manifest.636b4a7b4b2f0d4062e5872175f90b7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b2e99ceea3dd733fc6f",
+    "url": "/react-hooks/static/css/main.a833d7fc.chunk.css"
+  },
+  {
+    "revision": "6b2e99ceea3dd733fc6f",
+    "url": "/react-hooks/static/js/main.6b2e99ce.chunk.js"
+  },
+  {
+    "revision": "e5f1bd10652fc6b90e34",
+    "url": "/react-hooks/static/js/1.e5f1bd10.chunk.js"
+  },
+  {
+    "revision": "c0555af0d0302f662e6a",
+    "url": "/react-hooks/static/js/runtime~main.c0555af0.js"
+  },
+  {
+    "revision": "7c95537b9932ceeccc0ff5330f87c2bb",
+    "url": "/react-hooks/index.html"
+  }
+];
